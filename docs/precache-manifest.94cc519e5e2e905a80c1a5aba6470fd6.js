90b20e206a804bd0c41bd3e0beec216d1e4f8e3a
Samuel Gonzalez <xame2005@gmail.com>
2020-09-28 22:22:00 -0500

    [CREATED] Docs Folder

---
+++ docs/precache-manifest.94cc519e5e2e905a80c1a5aba6470fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c03b142bb977450cae768727310ad9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6890e867a67fc0be3b9",
+    "url": "/static/css/main.16a27bcd.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6890e867a67fc0be3b9",
+    "url": "/static/js/main.569c97d0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
