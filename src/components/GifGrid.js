90f22efdc9b9003fd48be1c10048a55e5d557a4d
Samuel Gonzalez <xame2005@gmail.com>
2020-09-28 20:07:11 -0500

    [CREATED] Custom Hooks for gif API request

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { GifGridItem } from "./GifGridItem";
 import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
+  // const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    getGifs(category).then(setImages);
-  }, [category]);
+  // useEffect(() => {
+  //   getGifs(category).then(setImages);
+  // }, [category]);
+
+  const { data, loading } = useFetchGifs(category);
+
+  console.log(data);
+  console.log(loading);
 
   return (
     <>
       <h3>{category}</h3>
+
+      {loading ? "Cargando..." : "Carga Finalizada"}
+
       <div className="card-grid">
-        {images.map((img) => (
+        {data.map((img) => (
           <GifGridItem key={img.id} {...img} />
         ))}
       </div>
